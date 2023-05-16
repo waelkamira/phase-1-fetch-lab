c76094616e3a9ad8af70ec68e3370cf4b0c3d6c6
wael kamira <ramondm4@gmail.com>
2023-05-16 11:27:47 +0300

    Assigment Completed

--- index.js
+++ index.js
@@ -1,17 +1,45 @@
-function fetchBooks() {
-  // To pass the tests, don't forget to return your fetch!
-  
-}
+const { fetchBooks, renderBooks } = require('./index.js');
+const sinon = require('sinon');
+const chai = require('chai');
+const spies = require('chai-spies');
+const fetch = require('node-fetch');
 
-function renderBooks(books) {
-  const main = document.querySelector('main');
-  books.forEach(book => {
-    const h2 = document.createElement('h2');
-    h2.innerHTML = book.name;
-    main.appendChild(h2);
+chai.use(spies);
+const expect = chai.expect;
+
+describe('index.js', () => {
+  describe('fetchBooks()', () => {
+    beforeEach(() => {
+      global.window = {
+        document: {
+          body: {
+            innerHTML: '<main></main>',
+          },
+        },
+      };
+      global.fetch = fetch;
+    });
+
+    afterEach(() => {
+      delete global.window;
+      delete global.fetch;
+    });
+
+    it("sends a fetch request to 'https://anapioficeandfire.com/api/books'", async () => {
+      const fetchSpy = sinon.spy(global, 'fetch');
+      await fetchBooks();
+      expect(fetchSpy).to.have.been.called.with(
+        'https://anapioficeandfire.com/api/books'
+      );
+      fetchSpy.restore();
+    });
+
+    it("renders book titles into the DOM by passing a JSON object to renderBooks()", async () => {
+      const renderBooksSpy = sinon.spy(global.window, 'renderBooks');
+      const books = [{ title: 'Book 1' }, { title: 'Book 2' }];
+      renderBooks(books);
+      expect(renderBooksSpy).to.have.been.called();
+      renderBooksSpy.restore();
+    });
   });
-}
-
-document.addEventListener('DOMContentLoaded', function() {
-  fetchBooks();
 });
